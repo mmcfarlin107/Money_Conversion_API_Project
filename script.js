a794f72106ba68dc8ccea0d1a32b8a922ca96500
mmcfarlin107 <mcfarlin107@gmail.com>
2017-10-05 22:38:25 -0400

    Started Layout.  Still needs more functionality.  API Call working.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
+//API Call for Currency conversion
+
+var promise = function(data){
+  console.log(data);
+};
 
 $.getJSON('http://api.fixer.io/latest', promise);
 
-var promise = function(data){
 
-}
 
 })
